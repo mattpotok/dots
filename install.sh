907a7aaee4b68ca5a4d0e192bca6d99b8e159230
Matthew Potok <protonic1@gmail.com>
2014-08-13 23:49:23 -0500

    Added RaspPi camera install

--- install.sh
+++ install.sh
@@ -14,13 +14,16 @@ homeDir="/home/$SUDO_USER"
 rpi=$(head -n 1 /etc/*-release | grep -cm1 Raspbian)
 
 echo "Installing packages"
-if [[ $rpi == 0 ]]
+if [[ $rpi == 1 ]]
 then
+  wget http://www.linux-projects.org/listing/uv4l_repo/lrkey.asc && sudo apt-key add ./lrkey.asc
+  echo "deb http://www.linux-projects.org/listing/uv4l_repo/raspbian/ wheezy main" >> /etc/apt/sources.list
+else
   echo "deb http://www.deb-multimedia.org/ wheezy main non-free" >> /etc/apt/sources.list
   echo "deb-src http://www.deb-multimedia.org/ wheezy main non-free" >> /etc/apt/sources.list
+  apt-get install -y --force-yes deb-multimedia-keyring
 fi
 
-apt-get install -y --force-yes deb-multimedia-keyring
 apt-get update && apt-get upgrade -y
 apt-get install -y vim tmux build-essential python-dev python-pip gdb git
 
@@ -31,6 +34,18 @@ then
   dpkg -i node_latest_armhf.deb
   git clone https://github.com/sarfata/pi-blaster.git
   cd pi-blaster && ./autogen && ./configure && make && sudo make install && cd ..
+  #Setting up pi camera
+  apt-get install libjpeg8-dev imagemagick libv4l-dev uv4l uv4l-raspicam uv4l-raspicam-extras
+  ln -s /usr/include/linux/videodev2.h /usr/include/linux/videodev.h
+   wget http://sourceforge.net/code-snapshots/svn/m/mj/mjpg-streamer/code/mjpg-streamer-code-182.zip
+   unzip mjpg-streamer-code-182.zip
+   cd mjpg-streamer-code-182/mjpg-streamer
+   make mjpg_streamer input_file.so output_http.so input_uvc.so
+   cp mjpg_streamer /usr/local/bin
+   cp output_http.so input_file.so /usr/local/lib/
+   cp -R www /usr/local/www
+   cd ../../
+   rm -rf mjpg-streamer-182
 else
   ./.opencv.sh
   git clone https://mpotok@bitbucket.org/mpotok/programs.git
